7cf50f1a8fc3e5ef7a81c05236391bb01d42c771
Samuel Akuma <usakuma@uwaterloo.ca>
2023-12-02 13:30:28 -0500

    base converter backend application

---
+++ src/test/java/com/phoenixtype/baseconverter/BaseconverterApplicationTests.java
@@ -0,0 +1,13 @@
+package com.phoenixtype.baseconverter;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BaseconverterApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
